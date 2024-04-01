384a111b0c1e965c926528c66ca664486f3092e0
Emeric Million <emeric.million@naval-group.com>
2024-04-01 21:53:13 +0200

    Return multiple results

--- rect-pos-from-search.py
+++ rect-pos-from-search.py
@@ -2,9 +2,24 @@ import pdb
 import fitz  # PyMuPDF
 
 
-def find_text_bbox(pdf_path, target_text):
+
+def find_checkboxes(page):
+    widgets = page.widgets()
+
+    # Iterate over each widget
+    for widget in widgets:
+        # Check if the widget is a checkbox
+        if widget.field_type == fitz.PDF_WIDGET_TYPE_CHECKBOX:
+            # Print the field name of the checkbox
+            print(f'Checkbox field name: {widget.field_name} position: {widget.rect}')
+
+
+def find_text_box(pdf_path, target_text, show_checkboxes=False):
     # Open the PDF
     doc = fitz.open(pdf_path)
+    
+    # Create an empty list to store all the rect positions
+    all_rects = []
     
     # Iterate over pages
     for page in doc:
@@ -14,21 +29,15 @@ def find_text_bbox(pdf_path, target_text):
         if target_text in text:
             # Get the rect positions of the text fragments containing the target text
             text_instances = page.search_for(target_text)
-            # Return the first rect position found
-            if text_instances:
-                return text_instances[0]
-                
-        widgets = page.widgets()
+            # Add all the rect positions to the list
+            all_rects.extend(text_instances)
 
-        # Iterate over each widget
-        for widget in widgets:
-            # Check if the widget is a checkbox
-            if widget.field_type == fitz.PDF_WIDGET_TYPE_CHECKBOX:
-                # Print the field name of the checkbox
-                print(f'Checkbox field name: {widget.field_name} position: {widget.rect}')
+        if show_checkboxes:
+            find_checkboxes(page)                
     
-    # Return None if the text is not found
-    return None
+    # Return all the rect positions
+    return all_rects
+
 
 def read_pdf(pdf_path):
     # Open the PDF
@@ -42,19 +51,20 @@ def read_pdf(pdf_path):
 
 if __name__ == "__main__":
     # Ask the user for PDF file path
-    default_pdf_path = r"C:\Users\emeric\dev\guso-autofill\blanked\FeuilletDeclaration.pdf"
+    default_pdf_path = r"./FeuilletDeclaration.pdf"
     pdf_path = input("Enter the path to the PDF file (leave empty for default): ").strip() or default_pdf_path
 
     
     # Ask the user for target text
     target_text = input("Enter the target text: ")
     
-    # Find the bounding box of the target text
-    rect = find_text_bbox(pdf_path, target_text)
-    
-    if rect:
-        print("Bounding box (Rect) of '{}' found:".format(target_text))
-        print("Position: ({}, {}, {}, {})".format(rect[0], rect[1], rect[2], rect[3]))
+    # Find the bounding boxes of the target text
+    rects = find_text_box(pdf_path, target_text)
+
+    if rects:
+        print("Bounding boxes (Rect) of '{}' found:".format(target_text))
+        for i, rect in enumerate(rects):
+            print("Bounding box {} position: ({}, {}, {}, {})".format(i+1, rect[0], rect[1], rect[2], rect[3]))
     else:
         print("Text '{}' not found in the PDF.".format(target_text))
 
@@ -65,15 +75,18 @@ if __name__ == "__main__":
 
         if choice == "interact":
             read_pdf(pdf_path)
-        elif choice == "redo a search":
+        elif choice.startswith("redo"):
             # Ask the user for another target text
-            new_target_text = input("Enter the new target text: ")
-            rect = find_text_bbox(pdf_path, new_target_text)
-            if rect:
-                print("Bounding box (Rect) of '{}' found:".format(new_target_text))
-                print("Position: ({}, {}, {}, {})".format(rect[0], rect[1], rect[2], rect[3]))
+            target_text = input("Enter the new target text: ")
+            # Find the bounding boxes of the target text
+            rects = find_text_box(pdf_path, target_text)
+
+            if rects:
+                print("Bounding boxes (Rect) of '{}' found:".format(target_text))
+                for i, rect in enumerate(rects):
+                    print("Bounding box {} position: ({}, {}, {}, {})".format(i+1, rect[0], rect[1], rect[2], rect[3]))
             else:
-                print("Text '{}' not found in the PDF.".format(new_target_text))
+                print("Text '{}' not found in the PDF.".format(target_text))
         elif choice == "quit":
             print("Exiting the script.")
             break
